bc44ce1be6037e5b55e758591d3b9588321a0892
Tony Wang <tony@initialcommit.net>
2022-03-19 13:50:30 +0800

    use python3

--- z.fish
+++ z.fish
@@ -7,9 +7,9 @@ function z
     end
     set dir (dirname (status -f))
     if [ $argv[1] = "--add" ]
-        python $dir/z.py $argv
+        python3 $dir/z.py $argv
     else
-        set -l dir (python $dir/z.py $argv)
+        set -l dir (python3 $dir/z.py $argv)
         cd $dir
     end
 end
